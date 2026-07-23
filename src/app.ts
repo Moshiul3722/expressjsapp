4beb8ee495d5d10763c80b68dbfb373836489db6
moshiul3722 <gazimoshiul@gmail.com>
2026-07-23 16:51:52 +0600

    2307202601

--- src/app.ts
+++ src/app.ts
@@ -8,9 +8,11 @@ import { userRoute } from "./modules/user/user.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { authRoute } from "./modules/auth/auth.route";
 import logger from "./middleware/logger";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.text());
 app.use(
@@ -18,8 +20,8 @@ app.use(
     extended: true,
   }),
 );
-
 app.use(logger);
+
 
 app.get("/", (req: Request, res: Response) => {
   // res.send('Hello World! he')
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import { pool } from "../../db";
-import jwt from "jsonwebtoken";
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import config from "../../config";
 
 const loginUserIntoDB = async (payload: {
@@ -39,7 +39,7 @@ const loginUserIntoDB = async (payload: {
   const accessToken = jwt.sign(jwtpayload, config.secret as string, {
     expiresIn: "1d",
   });
-  
+
   const refreshToken = jwt.sign(jwtpayload, config.refresh_secret as string, {
     expiresIn: "1d",
   });
@@ -47,6 +47,50 @@ const loginUserIntoDB = async (payload: {
   return { accessToken, refreshToken };
 };
 
+const generateRefreshToken = async (token: string) => {
+  if (!token) {
+    throw new Error("Unauthorized Access!!");
+  }
+
+  const decoded = jwt.verify(
+    token as string,
+    config.refresh_secret as string,
+  ) as JwtPayload;
+
+  const userData = await pool.query(
+    `
+        SELECT * FROM users WHERE email=$1
+        `,
+    [decoded.email],
+  );
+
+  // console.log(userData);
+  const user = userData.rows[0];
+
+  if (userData.rows.length === 0) {
+    throw new Error("User not found!");
+  }
+
+  if (!user.is_active) {
+    throw new Error("Forbidden!");
+  }
+
+  // 3. generate token
+  const jwtpayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    is_active: user.is_active,
+  };
+
+  const accessToken = jwt.sign(jwtpayload, config.secret as string, {
+    expiresIn: "1d",
+  });
+
+  return { accessToken };
+}
+
 export const authService = {
   loginUserIntoDB,
+  generateRefreshToken
 };
